4ba96f7047e60b461c6603e73ceff729e10cc005
Nicobauta <80293519+Nicobauta@users.noreply.github.com>
2026-02-18 15:46:04 -0500

    menu,y niveles

--- The Hunt/Assets/_Script/Platformer/PlayerMovement.cs
+++ The Hunt/Assets/_Script/Platformer/PlayerMovement.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private OldInput _oldInput;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("bonjour le monde");
+        _oldInput = GetComponent<OldInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
 
-   
+    public void Movement()
+    {
+        transform.Translate(Vector3.right * speed *
+            _oldInput.horizontal * Time.deltaTime);
+    }
 }
